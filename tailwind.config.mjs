cb65ec88fbe81e49938991a544ff92fc57af9f81
Cristian Rodriguez <cristiandosespigas@gmail.com>
2024-01-04 15:43:49 -0300

    se cambio las features

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,7 @@ export default {
 		extend: {
 			colors: {
 					'bunker': {
-						'50': '#e3e3e370',
+						'50': '#fcfbfc',
 						'100': '#eceef2',
 						'200': '#d5dae2',
 						'300': '#b0b9c9',
@@ -15,7 +15,7 @@ export default {
 						'500': '#667691',
 						'600': '#515f78',
 						'700': '#434d61',
-						'800': '#3a4252',
+						'800': '#fdfcfc',
 						'900': '#1e206f',
 						'950': '#ffffff',
 				},
@@ -24,6 +24,9 @@ export default {
 						'100': '#00000f'
 					}
 			},
+			boxShadow: {
+        'personalizado': '1px 4px 20px -2px rgb(0 0 0 / 10%)',
+      }
 		
 
 		},
